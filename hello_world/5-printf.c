f3c2bf5df227fbcc9068f98de47b9cf0c4fc3adb
Elvinooooo <Elvinooooo@github.com>
2023-10-14 09:04:46 -0700

    task 5 Hello,printf is completed

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+  *
+   * Description: This function is the entry point of the program. It prints
+    * a specific message to the standard output.
+     *
+      * Return: 0 to indicate successful execution.
+       */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
