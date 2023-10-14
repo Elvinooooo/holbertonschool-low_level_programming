814ecd929b8a60d0d1f721552745801861013a31
Elvinooooo <Elvinooooo@github.com>
2023-10-13 18:09:42 -0700

    4th task upd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
+/**
+*main - Prints "Programming is like building a multilingual
+*                puzzle, followed by a new line.
+*
+* Return: Always 0.
+*/
 int main(void)
-/*
- *4main -Entry point 5
- *6
- *Return: Always 0 (Success) 7
- */
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
-}
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+i}
