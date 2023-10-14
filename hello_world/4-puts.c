33902dfb04952c78d8e1f4074864cfd4f603643c
Elvinooooo <Elvinooooo@github.com>
2023-10-13 17:49:43 -0700

    upda

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
-int main(void)	
+int main(void)
+/** 4
+* main - Entry point 5
+* 6
+* Return: Always 0 (Success) 7
+ */
 {
-	/*We did it without printfn  */
-	puts("Programming is like building a multilingual puzzle");
+	puts("programming is like building a multilingual puzzle");
 	return (0);
 }
