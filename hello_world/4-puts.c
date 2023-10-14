e7b2ac1844bd8311cdb180371ac9073da9da2b41
Elvinooooo <Elvinooooo@github.com>
2023-10-13 17:57:12 -0700

    bugs fixed

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,6 +6,6 @@ int main(void)
  *Return: Always 0 (Success) 7
  */
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
