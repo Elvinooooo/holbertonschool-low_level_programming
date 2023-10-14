749b007c5ffaf86039479c12511fb4b6fafe2c27
Elvinooooo <Elvinooooo@github.com>
2023-10-13 17:55:11 -0700

    commited

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 int main(void)
-/** 4
-* main - Entry point 5
-* 6
-* Return: Always 0 (Success) 7
+/*
+ *4main -Entry point 5
+ *6
+ *Return: Always 0 (Success) 7
  */
 {
 	puts("programming is like building a multilingual puzzle");
