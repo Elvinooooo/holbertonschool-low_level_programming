abf53d6d4196dd8b3c8e4fc3f1ed68688e5e153f
Elvinooooo <Elvinooooo@github.com>
2023-10-14 10:10:40 -0700

    last task

---
+++ hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: this program is printing with the help of putchar function
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i;
+
+	for (i = 0; i <= 58; i++)
+	{
+		putchar(string[i]);
+	}
+
+	return (1);
+}
