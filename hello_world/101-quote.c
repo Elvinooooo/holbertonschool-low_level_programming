b2013f82c73be043d34f9d3776ed3df5417e068f
Elvinooooo <Elvinooooo@github.com>
2023-10-14 10:31:57 -0700

    fixed betty errors

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,9 +9,8 @@
 
 int main(void)
 {
-	char string[] = "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n";
-
+	char string[] = "and that piece of art is useful\"- Dora Korpar,\
+			2015-10-19\n";
 	int i;
 
 	for (i = 0; i <= 58; i++)
