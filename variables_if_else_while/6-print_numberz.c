036047c820f3c3433107561f5ba0eac76df04e73
Elvinooooo <Elvinooooo@github.com>
2023-10-16 10:06:00 -0700

    Task 6 so printing numbers using putchar completed

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+*Description: This function prints the base numbers from 0 to 10
+* using only putchar method twice
+*
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
 }
