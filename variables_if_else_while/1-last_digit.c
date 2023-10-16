6367d3fc40aa4a708f2238643787649e7b6f9b72
Elvinooooo <Elvinooooo@github.com>
2023-10-16 09:32:42 -0700

    the second task is completed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+* Description: The program generates a random number, then takes
+* the last digit of the generated number and prints the comparison of it with 5
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-	return
+	int n;
+	int modulus;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	modulus = n % 10;
+	if (modulus > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, modulus);
+	else if (modulus == 0)
+		printf("Last digit of %d is %d and is 0\n", n, modulus);
+	else if (modulus < 6 && modulus != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, modulus);
+	return (0);
 }
