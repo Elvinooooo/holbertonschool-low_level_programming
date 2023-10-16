939ff2ae0fe047542e66fa437a478f4922b4c26e
Elvinooooo <Elvinooooo@github.com>
2023-10-16 09:51:55 -0700

    Task4 so printing letters except q and e completed

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+* Description: This function prints all alphabet letters in lowercase
+* exeptc q and q letter. The putchar method can bes used only twice..
+*
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'q' || alphabet == 'e')
+			alphabet++;
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
 }
