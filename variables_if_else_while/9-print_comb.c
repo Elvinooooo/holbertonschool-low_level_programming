02b40b33368b4f9e3a576fe2b528bf8127fdec5c
Elvinooooo <Elvinooooo@github.com>
2023-10-16 10:45:32 -0700

    9th task so printing single digit numbers separated by comma and space is completed

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+*Description: This function prints the single digit numbers separated
+*by comma and whitespace using only putchar method four times
+*
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
 }
