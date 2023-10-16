3c73bbd4c0b7c966161d8d0c725b1860a84d7bb0
Elvinooooo <Elvinooooo@github.com>
2023-10-16 10:24:49 -0700

    8th task so printing 16 base numbers is completed

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+*Description: This function prints all the number of base 16
+* using only putchar method three times
+*
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
