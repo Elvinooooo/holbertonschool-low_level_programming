29dfda75ee96fc994ff3ab2bc8a2ef6c45c2d335
Elvinooooo <Elvinooooo@github.com>
2023-10-16 09:40:33 -0700

    Task3 so printing alphabet letters is completed

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,19 @@
  * main - Entry point of the program
    *
       * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
+* a specific message to the standard output.
 	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
 }
