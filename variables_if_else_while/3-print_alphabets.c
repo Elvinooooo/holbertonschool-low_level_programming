481a5ad34be1dc0890154212ba672834f62e59da
Elvinooooo <Elvinooooo@github.com>
2023-10-16 09:46:11 -0700

    Alphabet in lowercase and then uppercase is completed

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
-   *
-      * Description: This function is the entry point of the program. It prints
-          * a specific message to the standard output.
-	       *
-	             * Return: 0 to indicate successful execution.
-		            */
+* main - Entry point of the program
+*
+* Description: This function prints alphabet letters in lowercase
+* then in uppercase only with using putchar method..
+*
+* Return: 0 to indicate successful execution.
+*/
 int main(void)
 {
-	return
+	char alphabet = 'a';
+	char alphabet2 = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	while (alphabet2 <= 'Z')
+	{
+		putchar(alphabet2);
+		alphabet2++;
+	}
+	putchar('\n');
+	return (0);
 }
