8618379c8ccbfd85ebf59367b3258a3091fc7685
Elvinooooo <Elvinooooo@github.com>
2023-10-17 15:37:30 -0700

    the addition function task completed

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* add - Function that adds two integer
+*
+* @a: the first parameter;
+* @b: the second parameter;
+*
+* Return: returns the result of addition of two integers
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+
+}
