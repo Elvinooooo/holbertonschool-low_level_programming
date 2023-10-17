49567694370cf42579b2e00d565b8eab4fc63f5f
Elvinooooo <Elvinooooo@github.com>
2023-10-16 23:34:48 -0700

    second task should be done

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - function prints albhabet letters 10 times
+* followed by a new line
+*
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+}
