0bcd384b7cd7fcdf526dbc38fdd8ab747e312f18
Elvinooooo <Elvinooooo@github.com>
2023-10-16 22:49:04 -0700

    first task

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -  Entry Point of the program that mentions the function prototype
+ *
+ *Description: function prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+main(void)
+{
+	_putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('a');
+	putchar('\n');
+	return (0);
+}
