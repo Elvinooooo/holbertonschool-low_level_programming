0b3b0aa3603f1195cf97e111bbfdbb1c95d5e15b
Elvinooooo <Elvinooooo@github.com>
2023-10-17 13:14:43 -0700

    printing hour seconds

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_last_digit  - prints the last digit of a number...
+ *
+ *@a: The argument of the function to be checked
+ *
+ * Return: the value of the last digit
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
