bb7954b534fa2660c5368e3c25a4e9060cc7ba5f
Elvinooooo <Elvinooooo@github.com>
2023-10-16 22:55:52 -0700

    added underscore before the function

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	_putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('a');
-	putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	_putchar('\n');
 	return (0);
 }
