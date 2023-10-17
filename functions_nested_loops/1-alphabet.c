0d4c495fed1fd7f2c473c2abd214b09ff8eaaeeb
Elvinooooo <Elvinooooo@github.com>
2023-10-16 23:18:03 -0700

    first task completed

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,9 +7,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	int i = 'a';
+
+	char i = 'a';
 
 	while (i <= 'z')
 	{
@@ -17,5 +19,4 @@ int main(void)
 		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
