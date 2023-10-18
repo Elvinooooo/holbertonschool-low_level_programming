d460790af94fd8613ec1a1bf48b8f7ef03f38205
Elvinooooo <Elvinooooo@github.com>
2023-10-18 05:23:20 -0700

    updated

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
+*print_times_table - function that prints time table of the given number
 *
-*
+*@n: the given number for the apropriate time table
 */
 void print_times_table(int n)
 {
-	if(n > 15 || n < 0)
-		return ;
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
 	else
 	{
 		int i = 0;
@@ -16,11 +19,19 @@ void print_times_table(int n)
 
 		for (; i <= n ; i++)
 		{
-			for(; j <= n; j++)
+			for (; j <= n; j++)
 			{
 				result = i * j;
-				printf("%d,  ", result);
-			}		
+				if (j == n)
+				{
+					printf("%d", result);
+				}
+				else
+				{
+					 printf("%d, ", result);
+				}
+			}
+			printf("\n");
 		}
 	}
 }
