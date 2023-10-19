8cf1373484c1bfb6fc2d27df02828fb85f4a6cd5
Elvinooooo <Elvinooooo@github.com>
2023-10-19 14:45:49 -0700

    task 4 print most numbers completed

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers -from 0 to 9, followed by a new line.
+* Do not print 0 and 4
+*
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if(i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
