f7033597ee89169e47a7acdb6f9ea78fd32f3409
Elvinooooo <Elvinooooo@github.com>
2023-10-20 06:38:33 -0700

    changed code for the correct output. assigned variable with unsigned integer in order to correct the output of int_min

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,14 @@
 
 void print_number(int n)
 {
+	int unsigned int number = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (number / 10)
+		print_number(number / 10);
+	_putchar((number % 10) + '0');
 }
