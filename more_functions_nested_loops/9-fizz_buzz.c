1349807997e12ea2c105739c07cdcf64371e248f
Elvinooooo <Elvinooooo@github.com>
2023-10-20 00:37:35 -0700

    corrected the syntax fo FizzBuzz from Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf(" Fizz Buzz");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
 			printf(" Fizz");
 		else if (i % 5 == 0)
