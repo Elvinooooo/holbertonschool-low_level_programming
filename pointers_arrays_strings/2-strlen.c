82604e4090bf3658cd66f222c294faae1dc85f8c
Elvinooooo <Elvinooooo@github.com>
2023-10-23 04:48:06 -0700

    task 3 printing

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* _strlen - Function that function that returns the length of a string.
+*_strlen - Function that function that returns the length of a string.
 *@s: The char pointer argument
 *
-* Return - the length of the string
+* Return: the length of the string
 */
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*s)
 	{
 		s++;
