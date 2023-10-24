a1345ad95ad46516930311ae252dfa37a78e9f2c
Elvinooooo <Elvinooooo@github.com>
2023-10-24 09:12:40 -0700

    8th task printing array completed

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array - Function that prints n elements of an array of integers
+*followed by a new line.
+*@a: The given array argument
+*@n: is the number of elements of the array to be printed
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d \n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
