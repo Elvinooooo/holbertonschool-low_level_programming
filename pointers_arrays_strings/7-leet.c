d4a7b56e52f7cb55ab622e97dfb0c9ba8b87ad19
Elvinooooo <Elvinooooo@github.com>
2023-10-25 01:23:49 -0700

    added 7th task

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*leet - function that encodes a string into 1337
+*@str: argument of the function
+*
+*Return: the character;
+*/
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if(str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if(str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str
+		i++;
+	}
+}
