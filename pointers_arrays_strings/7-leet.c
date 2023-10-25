94a7be3da640f8c205c1e7536cf1bd205c3090dd
Elvinooooo <Elvinooooo@github.com>
2023-10-25 05:23:43 -0700

    last task

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,11 +11,17 @@ char *leet(char *str)
 
 	while (str[i])
 	{
-		if(str[i] == 'a' || str[i] == 'A')
+		if (str[i] == 'a' || str[i] == 'A')
 			str[i] = '4';
-		else if(str[i] == 'e' || str[i] == 'E')
+		else if (str[i] == 'e' || str[i] == 'E')
 			str[i] = '3';
-		else if (str
+		else if (str[i] == 'O' || str[i] == 'o')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
 		i++;
 	}
+	return (str);
 }
