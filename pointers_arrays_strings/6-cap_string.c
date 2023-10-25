44a68676defffc7ade510041254676334841f71f
Elvinooooo <Elvinooooo@github.com>
2023-10-25 01:03:24 -0700

    capitalize string task completed

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
-*string_toupper -that changes all lowercase letters of a string to uppercase
+*cap_string -function that capitalize all words of a strin
 *@str: string argument
 *
-*Return: the converted to uppercase string
+*Return: the converted capitalized string
 */
 char *cap_string(char *str)
-    {
-        int i = 0;
+{
+	int i = 0;
 
-        while (str[i])
-        {
-               while (!(str[i] <= 'z' &&  str[i] >= 'a'))
-                   i++;
-                if (  str [i - 1] == ' ' || str[i - 1 ] == '\n' || i == 0 || str[i - 1] == '}' || str[i - 1] == '}' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' 
-                || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '.')
-                {
-                        str[i] -= 32;
-                }
-                i++;
-        }
-        return (str);
-    }
+	while (str[i])
+	{
+		while (!(str[i] <= 'z' &&  str[i] >= 'a'))
+			i++;
+		if (str[i - 1] == ' ' || str[i - 1] == '\n' || i == 0 ||
+		str[i - 1] == '}' || str[i - 1] == '}' || str[i - 1] == ',' ||
+		str[i - 1] == ';' || str[i - 1] == '!' || str[i - 1] == '?' ||
+		str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		str[i - 1] == '.')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
