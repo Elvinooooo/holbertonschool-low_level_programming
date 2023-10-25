a2da922b6d56f9d81a70e57cc99df16f0e7e21f7
Elvinooooo <Elvinooooo@github.com>
2023-10-25 06:07:51 -0700

    removed the additional loops and keep only 2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,34 +7,19 @@
 */
 char *leet(char *str)
 {
-	int i = 0;
+	int i, j;
+	char replacedChars[] = "AaEeOoTtLl";
+	char replacingNums[] = "4433007711";
 
+	i = 0;
 	while (str[i])
 	{
-		while (str[i] == 'a' || str[i] == 'A')
+		j = 0;
+		while (replacedChars[j])
 		{
-			str[i] = '4';
-			break;
-		}
-		while (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-			break;
-		}
-		while (str[i] == 'O' || str[i] == 'o')
-		{
-			str[i] = '0';
-			break;
-		}
-		while (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-			break;
-		}
-		while (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-			break;
+			if (str[i] == replacedChars[j])
+				str[i] = replacingNums[j];
+			j++;
 		}
 		i++;
 	}
