4b550d1b8ded0461861ebbbfd82e057eea652cc5
Elvinooooo <Elvinooooo@github.com>
2023-10-28 13:52:52 -0700

    code updated

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,8 +13,9 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
+	{
 		sum1 += a[i * size + i];
-	for (; i >= 0; i--)
-		sum2 += a[i * size - i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
