b5a642f9a026d93f08e5f1d44a47c5d904139863
Elvinooooo <Elvinooooo@github.com>
2023-11-03 06:37:46 -0700

    betty style adjusted. added comments inside

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+*main - function that prints the name of the function
+*@argc: count of the arguments
+*@argv: array of string including name of function
 *
+*Return: 0 for stopping the program
 */
 int main(int argc, char *argv[])
 {
