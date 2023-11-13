974546e474aa77acd339d072dfb3342d0685f453
Elvinooooo <Elvinooooo@github.com>
2023-11-13 10:44:00 -0800

    rot 13 done

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*rot13 -  function that encodes a string using rot13
+*@str: the string that should be encoded
+*Return: The encoded string
+*/
+char *rot13(char *str)
+{
+	while (str[i])
+	{
+		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			char base = (str[i] >= 'a') * ('a' - 1) + (str[i] < 'a') * ('A' - 1) + 1;
+
+			str[i] = (((str[i] - base + 13) % 26) + base);
+		}
+		i++;
+	}
+	return (str);
+}
