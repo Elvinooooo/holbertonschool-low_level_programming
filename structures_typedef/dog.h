c977a495b15bc4ce9d9366fe5a94beb2548a0d31
Elvinooooo <Elvinooooo@github.com>
2023-11-20 07:52:34 -0800

    comment added for dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - dog describing struct
+* @name: the name of the dog
+* @age: float age of the dog
+* @owner: The name of the owner of the dog
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
