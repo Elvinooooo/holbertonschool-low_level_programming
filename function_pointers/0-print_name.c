a660ce749ffeeb5b24d5731905095c5032cb27cf
Elvinooooo <Elvinooooo@github.com>
2023-11-23 04:13:24 -0800

    betty adjusted

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,12 +3,13 @@
 /**
 * print_name -  function that prints given name
 * @name: the name to be printed
+* @f: function pointer
 * Return : nothing
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL)
+	if (name != NULL || f != NULL)
 	{
 		f(name);
 	}
