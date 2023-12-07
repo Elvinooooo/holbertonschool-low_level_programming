9c6235813d31690c53f46a01d16363d2c957a9b4
Elvinooooo <Elvinooooo@github.com>
2023-12-07 11:01:37 -0800

    sum of all data

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* sum_dlistint -  function that returns the sum of all the data in list.
+* @head: the first element of the linked list
+* Return: The sum of the integers
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head;
+		head =  head->next;
+	}
+	return (sum);
+}
