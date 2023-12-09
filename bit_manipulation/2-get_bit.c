86f3e5d86318ee7a3d20493a039943e71cee5ede
Elvinooooo <Elvinooooo@github.com>
2023-12-09 12:26:47 -0800

    task 2 and 3 done

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @index: the index, starting from 0 of the bit you want to get
+ * @n: the fiven number
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int binary[128];
+	unsigned int i = 0;
+
+	while (n > 0)
+	{
+		binary[i] =  n % 2;
+		n /= 2;
+		i++;
+	}
+	if (index < i)
+		return (binary[index]);
+	else
+		return (-1);
+}
