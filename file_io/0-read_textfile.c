2688b238bd7ba878969549439a4c4cc88420eefb
Elvinooooo <Elvinooooo@github.com>
2023-12-12 07:53:21 -0800

    size_t changed to ssize_t

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  *@letters: the number of letters it should read and print
  *Return: the actual number of letters it could read and print
  */
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char * buffer;
 	ssize_t o;
