b2a4907ddb3cf80080f2eacc86ef0417848622aa
Elvinooooo <Elvinooooo@github.com>
2023-12-12 11:44:39 -0800

    added O_RDWR flag

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while(text_content[len])
 		len++;
-	fd = open(filename, O_CREAT | O_TRUNC, 0644);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fd == -1)
 	{
 		return (-1);
