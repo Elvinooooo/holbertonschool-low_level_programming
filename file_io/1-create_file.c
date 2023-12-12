50d5c4db5287809496564f2c1f22e889589fb6ba
Elvinooooo <Elvinooooo@github.com>
2023-12-12 11:47:10 -0800

    added condition if text_content == NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,8 +12,11 @@ int create_file(const char *filename, char *text_content)
 	len = 0;
 	if (filename == NULL)
 		return (-1);
-	while(text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		while(text_content[len])
+			len++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fd == -1)
 	{
