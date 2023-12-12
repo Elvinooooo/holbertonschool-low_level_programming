b7df4ca9d37ba9359c1b6cbf56bee3132e1ac8a7
Elvinooooo <Elvinooooo@github.com>
2023-12-12 11:08:20 -0800

    the task one read_textfile completed

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,32 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *read_textfile - function that reads a text file and prints it to the POSIX standard output
- *filename: Pointer to the name of file
+ *read_textfile - function that reads a text file and prints
+ *it to the POSIX standard output
+ *@filename: the name of the file to be read
  *@letters: the number of letters it should read and print
- *Return: the actual number of letters it could read and print
+ *Return:  the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char * buffer;
-	ssize_t o;
-	ssize_t r;
-	ssize_t w;
+	char *buffer;
+	ssize_t fd, fd1, fd2;
 
-	if (filename  == NULL)
+	if (filename == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buffer =  malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY);
+	fd1 = read(fd, buffer, letters);
+	fd2 = write(1, buffer, fd1);
+	if (fd == -1 || fd1 == -1 || fd2 == -1)
+	{	free(buffer);
 		return (0);
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-	if (o == -1 || r == -1 || w == -1 || w!= r)
-	{
-		free(buffer);
-		return(0);
 	}
 	free(buffer);
-	close(o);
-	return (w);
+	close(fd);
+	return (fd2);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,4 +1,8 @@
 #ifndef MAIN_C
 #define MAIN_C
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
