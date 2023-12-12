3a830834059afbf128de43d592f11403e1a9e74e
Elvinooooo <Elvinooooo@github.com>
2023-12-12 11:42:19 -0800

    upd

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,10 +15,15 @@ int create_file(const char *filename, char *text_content)
 	while(text_content[len])
 		len++;
 	fd = open(filename, O_CREAT | O_TRUNC, 0644);
-	fd1 = write(fd, text_content, len);
-	if (fd == -1 || fd1 == -1)
+	if (fd == -1)
 	{
 		return (-1);
+	}
+	fd1 = write(fd, text_content, len);
+	if (fd1 == -1)
+	{
+		close(fd);
+		return(-1);
 	}
 	close(fd);
 	return (1);
