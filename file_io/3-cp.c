86b3a08d31cfdc2eabd7dc74bab76b37caba79b7
Elvinooooo <Elvinooooo@github.com>
2023-12-13 12:54:53 -0800

    updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,19 +13,45 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		exit(dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n") || 97);
-	if ((open1 = open(argv[1], O_RDONLY)) == -1)
-		exit(dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]) || 98);
-	if ((open2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664)) == -1)
-		exit(close(open1) || dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]) || 99);
-	while ((read1 = read(open1, buffer, sizeof(buffer))) > 0)
 	{
-		if ((write1 = write(open2, buffer, read1)) == -1)
-			exit(close(open1) || close(open2) || dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]) || 99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open1 = open(argv[1], O_RDONLY);
+	if (open1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	open2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (open2 == -1)
+	{
+		close(open1);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	while ((read1 = read(open1, buffer, 1024)) > 0)
+	{
+		write1 = write(open2, buffer, read1);
+		if (write1 == -1)
+		{
+			close(open1);
+			close(open2);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	if (read1 == -1)
-		exit(close(open1) || close(open2) || dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]) || 98);
+	{
+		close(open1);
+		close(open2);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(open1) == -1 || close(open2) == -1)
-		exit(dprintf(STDERR_FILENO, "Error: Can't close fd\n") || 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
 	return (0);
 }
