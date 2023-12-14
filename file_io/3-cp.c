88407ff68d13b78ce68be13aa85da0292e4d1d37
Elvinooooo <Elvinooooo@github.com>
2023-12-14 08:22:05 -0800

    updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,7 +7,7 @@
  * @source: The source of the error (void pointer to handle any type)
  * @exit_code: The exit code
  */
-void print_and_exit(char *message, void *source, int exit_code)
+void print_and_exit(char *message, int source, int exit_code)
 {
 	dprintf(STDERR_FILENO, message, source);
 	exit(exit_code);
@@ -25,15 +25,18 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		print_and_exit("Usage: cp file_from file_to\n", NULL, 97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	open1 = open(argv[1], O_RDONLY);
 	if (open1 == -1)
-		print_and_exit("Error: Can't read from file %s\n", argv[1], 98);
+		print_and_exit("Error: Can't read from file %s\n", atoi(argv[1]), 98);
 	open2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (open2 == -1)
 	{
 		close(open1);
-		print_and_exit("Error: Can't write to file %s\n", argv[2], 99);
+		print_and_exit("Error: Can't write to file %s\n", atoi(argv[2]), 99);
 	}
 	while ((read1 = read(open1, buffer, 1024)) > 0)
 	{
@@ -42,18 +45,18 @@ int main(int argc, char *argv[])
 		{
 			close(open1);
 			close(open2);
-			print_and_exit("Error: Can't write to file %s\n", argv[2], 99);
+			print_and_exit("Error: Can't write to file %s\n", atoi(argv[2]), 99);
 		}
 	}
 	if (read1 == -1)
 	{
 		close(open1);
 		close(open2);
-		print_and_exit("Error: Can't read from file %s\n", argv[1], 98);
+		print_and_exit("Error: Can't read from file %s\n", atoi(argv[1]), 98);
 	}
 	if (close(open1) == -1)
 		print_and_exit("Error: Can't close fd%d\n", open1, 100);
 	if (close(open2) == -1)
-		print_and_exit("Error: Can't read from file %s\n", open2, 100);
+		print_and_exit("Error: Can't read from file %d\n", open2, 100);
 	return (0);
 }
