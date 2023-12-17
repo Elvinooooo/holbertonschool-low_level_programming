c416b72499f915b99e90e16bb22d28fe3c999844
Elvinooooo <Elvinooooo@github.com>
2023-12-17 07:52:22 -0800

    the comma problem in the fifth task solved

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,21 +7,29 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
-
 	unsigned long int i;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		node =  ht->array[i];
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			if (i < ht->size - 1 || node->next != NULL)
+			if (comma_flag == 1)
 				printf(", ");
-			node =  node->next;
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
 		}
 	}
 	printf("}\n");
