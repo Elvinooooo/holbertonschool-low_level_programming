d6c3ea22f5e3197f62e2e7fdf0f19dd4bc5c9e60
Elvinooooo <Elvinooooo@github.com>
2023-12-17 07:42:07 -0800

    5th corrected

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 		node =  ht->array[i];
 		while (node != NULL)
 		{
-			printf("'%s':'%s'", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			if (i < ht->size - 1 || node->next != NULL)
 				printf(", ");
 			node =  node->next;
